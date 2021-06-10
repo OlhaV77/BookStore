dbfd36c4ce3288dde23657f9f826c9c4c87e5771
olgaV77 <olga.v1977@gmail.com>
2021-06-10 16:14:22 -0400

    $ git  --author Olga --before"Wen. jun 10, 2021" add:   small menu
    1. scan method
    2. scan user input method
    3. show all books method

--- src/main/java/ListAllMaterials.java
+++ src/main/java/ListAllMaterials.java
@@ -7,10 +7,12 @@ public class ListAllMaterials {
     public static void main(String[] args) {
         BookStore subject = new BookStore();
 
+        subject.showAllBook();
+
         ArrayList<Author> authorsForFirstBook = new ArrayList<>();
         authorsForFirstBook.add(new Author("Tom", "Henz"));
 
-        Date date1 = new Date(2020,4,30);
+        Date date1 = new Date(2020, 4, 30);
 
         Book book1 = new Book("History",
                 5.55,
@@ -27,7 +29,7 @@ public class ListAllMaterials {
         authorsForSecondBook.add(new Author("Lee", "Keem"));
         authorsForSecondBook.add(new Author("Leo", "Tolstoy"));
 
-     Date date2 = new Date(2021, 3, 13);
+        Date date2 = new Date(2021, 3, 13);
 
         Book book2 = new Book("Math",
                 8.67,
@@ -41,14 +43,15 @@ public class ListAllMaterials {
         ArrayList<Author> thirdBookAuthor = new ArrayList<>();
         thirdBookAuthor.add(new Author("Lewis", "Carroll"));
 
-       Date date3 = new Date(2013, 2, 15);
+        Date date3 = new Date(2013, 2, 15);
 
         Book book3 = new Book("Art",
                 15.6,
                 date3,
                 thirdBookAuthor,
-                new ISBN("ISBN","2341-6754"));
+                new ISBN("ISBN", "2341-6754"));
         subject.add(book3);
+
 
         for (int i = 0; i < subject.items.size(); i++) {
             System.out.println(subject.items.get(i));
